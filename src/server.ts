385db2516fc957b5a2e898d62ea38db1adf5e4f5
“Rodrigo <rodrigobpzantunes2016@gmail.com>
2023-11-15 16:55:39 -0300

    test: supertest

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,5 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
-import { transactionsRoutes } from './routes/transactions'
+import { app } from './app'
 import { env } from './env'
-
-const app = fastify()
-
-app.register(cookie)
-app.register(transactionsRoutes)
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
 
 app
   .listen({
