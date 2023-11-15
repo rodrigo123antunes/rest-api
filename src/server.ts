45979acd6b3dd72429d8abfaa43905721c1c1f6a
“Rodrigo <rodrigobpzantunes2016@gmail.com>
2023-11-15 16:17:35 -0300

    feat: separate route in a file

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
+import { transactionsRoutes } from './routes/transactions'
 import { env } from './env'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transactions
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
